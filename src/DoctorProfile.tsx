592027f8b973891f9ff444930b96561ee31c0e24
Cinthia <cinthia.j.23@gmail.com>
2020-05-05 17:34:10 +1000

    MV-32 nav link working, with a bug with the nav select color

---
+++ src/DoctorProfile.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { RouteComponentProps } from '@reach/router';
+
+const DoctorProfile: React.FC<RouteComponentProps> = () => {
+    return <p>Hola soy la fotito!</p>;
+};
+
+export default DoctorProfile;
---
+++ src/History.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { RouteComponentProps } from '@reach/router';
+
+const History: React.FC<RouteComponentProps> = () => {
+    return <p>Hola soy historiaaa!</p>;
+};
+
+export default History;
