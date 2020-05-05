56d79c3df308766e12b7c70125e2a5a4074cc1a7
Cinthia <cinthia.j.23@gmail.com>
2020-05-05 21:45:03 +1000

    resultados de laboratorio listo

---
+++ src/components/LabResults.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LabResults from './LabResults';
+
+export default {
+    title: 'Resultados de Laboratorio',
+    component: LabResults,
+};
+
+export const Default = () => <LabResults />;
